29bc3738ea7427cd346602a07a69c434c53d5b7d
RafaelSempere <43506952+RafaelSempere@users.noreply.github.com>
2018-09-23 02:33:36 +0200

    Add files via upload

--- orabox-dashboard-theme.php
+++ orabox-dashboard-theme.php
@@ -2,12 +2,13 @@
 
 /*
 Plugin Name: Orabox Theme
-Plugin URI: https://rafaelsempere.com
+Plugin URI: https://github.com/RafaelSempere/Orabox
 Description: The theme that you were looking for.
 Author: Rafael Sempere
+Author URI: https://rafaelsempere.com
 Version: 1.1
 License: GPLv3 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
 
 function orabox_style() {
